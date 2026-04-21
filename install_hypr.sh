a623ea5be50b732cca8e316fdd7a254db2f04a71
Denis Rodin <denis.rodin@proton.me>
2026-04-21 20:37:47 +0200

    feat: Add swaylock

--- install_hypr.sh
+++ install_hypr.sh
@@ -3,6 +3,7 @@
 sudo pacman -Syu
 
 sudo pacman -S hyprland xdg-desktop-portal-hyprland hyprlock hypridle
+sudo pacman -S swaylock
 sudo pacman -S wl-clipboard grim slurp swaybg ttf-jetbrains-mono-nerd
 sudo pacman -S hyprlauncher hyprpaper hyprpolkitagent waybar hyprshot
 sudo pacman -S swaync # remove dunst if needed
