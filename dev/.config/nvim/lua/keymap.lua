3abbafaadd20bbed17e26797c7e0f99ec71b9ffa
Denis Rodin <denis.rodin@proton.me>
2026-04-24 23:51:55 +0200

    feat: Small nvim remaps

--- dev/.config/nvim/lua/keymap.lua
+++ dev/.config/nvim/lua/keymap.lua
@@ -3,4 +3,8 @@ vim.keymap.set("n", "<leader>e", "<Cmd>Explore<CR>")
 local fzf = require("fzf-lua")
 vim.keymap.set("n", "<leader><leader>", fzf.files)
 vim.keymap.set("n", "<leader>/", fzf.live_grep)
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
