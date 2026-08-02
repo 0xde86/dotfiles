a1c32bbf0f9d5f4f2354205072f7f3bd61106eaa
Denis Rodin <denis.rodin@proton.me>
2026-08-02 17:08:12 +0200

    feat: Use yazi as default file manager

--- desktop/.config/hypr/programs.lua
+++ desktop/.config/hypr/programs.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.terminal    = "kitty"
-M.fileManager = "cosmic-files"
+M.fileManager = "kitty --override confirm_os_window_close=0 -e yazi"
 M.menu        = "hyprlauncher"
 M.browser     = "brave --password-store=gnome"
 M.p_browser   = "flatpak run net.mullvad.MullvadBrowser"
